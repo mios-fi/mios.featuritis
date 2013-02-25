8a9362c44a4cb966b33dcba1a44c40c5c85300ea
kaa <jbeijar@mios.fi>
2013-02-25 21:37:57 +0200

    Add a generic lambda toggler and refactor roletoggler to use it.

---
+++ core/LambdaToggler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mios.Featuritis {
+	public class LambdaToggler : IToggler {
+		private readonly Func<bool> predicate;
+		private readonly IToggler wrapped;
+		public LambdaToggler(Func<bool> predicate, IToggler wrapped) {
+			this.predicate = predicate;
+			this.wrapped = wrapped;
+		}
+		public IEnumerable<string> Toggles {
+			get {
+				return predicate()
+					? wrapped.Toggles
+					: Enumerable.Empty<string>();
+			}
+		}
+		public bool? IsEnabled(string toggle) {
+			return predicate()
+				? wrapped.IsEnabled(toggle)
+				: null;
+		}
+	}
+}
---
+++ tests/LambdaTogglerTests.cs
@@ -0,0 +1,52 @@
+﻿using Mios.Featuritis;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Web;
+using Xunit;
+
+namespace tests {
+	public class LambdaTogglerTests {
+		private IToggler wrapped;
+		public LambdaTogglerTests() {
+			this.wrapped = Mock.Of<IToggler>(t =>
+				t.IsEnabled("a") == true &&
+				t.IsEnabled("b") == false &&
+				t.Toggles == new[] { "a", "b" }
+			);
+		}
+
+		[Fact]
+		public void EnumeratesWrappedTogglesWhenInFunctionReturnsTrue() {
+			var toggler = new LambdaToggler(() => true, wrapped);
+			var toggles = toggler.Toggles.ToArray();
+			Assert.Equal(2, toggles.Length);
+			Assert.Contains("a", toggles);
+			Assert.Contains("b", toggles);
+		}
+
+		[Fact]
+		public void ReturnsEmptyEnumerableWhenFunctionReturnsFalse() {
+			var toggler = new LambdaToggler(() => false, wrapped);
+			Assert.Empty(toggler.Toggles);
+		}
+
+		[Fact]
+		public void ReturnsWrappedToggleValuesWhenFunctionReturnsTrue() {
+			var toggler = new LambdaToggler(() => true, wrapped);
+			Assert.Equal(true, toggler.IsEnabled("a"));
+			Assert.Equal(false, toggler.IsEnabled("b"));
+			Assert.Equal(null, toggler.IsEnabled("x"));
+		}
+
+		[Fact]
+		public void ReturnsUndefinedWhenFunctionReturnsFalse() {
+			var toggler = new LambdaToggler(() => false, wrapped);
+			Assert.Equal(null, toggler.IsEnabled("a"));
+		}
+	}
+}
